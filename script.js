4b37fa5ea8c44f4f03f89d038b81e1a5a045ac7d
Jens Baumeister <jens.baumeister@gmx.net>
2026-01-23 15:36:49 +0100

    code formatted

--- script.js
+++ script.js
@@ -1,75 +1,75 @@
 let cart = [];
 
 function init() {
-    renderMainDishes();
-    renderDrinks();
-    renderDesserts();
-    renderCart();
+  renderMainDishes();
+  renderDrinks();
+  renderDesserts();
+  renderCart();
 }
 
 function renderMainDishes() {
-    let mainDishesRef = document.getElementById('maindishes');
-    mainDishesRef.innerHTML = '';
-    for (let i = 0; i < menu.mainDishes.length; i++) {
-        mainDishesRef.innerHTML += getMainDishes(i);
-    }
+  let mainDishesRef = document.getElementById("maindishes");
+  mainDishesRef.innerHTML = "";
+  for (let i = 0; i < menu.mainDishes.length; i++) {
+    mainDishesRef.innerHTML += getMainDishes(i);
+  }
 }
 
 function renderDrinks() {
-    let drinksRef = document.getElementById('drinks');
-    drinksRef.innerHTML = '';
-    for (let i = 0; i < menu.drinks.length; i++) {
-        drinksRef.innerHTML += getDrinks(i);
-    }
+  let drinksRef = document.getElementById("drinks");
+  drinksRef.innerHTML = "";
+  for (let i = 0; i < menu.drinks.length; i++) {
+    drinksRef.innerHTML += getDrinks(i);
+  }
 }
 
 function renderDesserts() {
-    let dessertsRef = document.getElementById('desserts');
-    dessertsRef.innerHTML = '';
-    for (let i = 0; i < menu.desserts.length; i++) {
-        dessertsRef.innerHTML += getDesserts(i);
-    }
+  let dessertsRef = document.getElementById("desserts");
+  dessertsRef.innerHTML = "";
+  for (let i = 0; i < menu.desserts.length; i++) {
+    dessertsRef.innerHTML += getDesserts(i);
+  }
 }
 
 function onlyDishes() {
-    document.getElementById('maindishes').innerHTML = '';
-    document.getElementById('drinks').innerHTML = '';
-    document.getElementById('desserts').innerHTML = '';
-    renderMainDishes();
+  document.getElementById("maindishes").innerHTML = "";
+  document.getElementById("drinks").innerHTML = "";
+  document.getElementById("desserts").innerHTML = "";
+  renderMainDishes();
 }
 
 function onlyDrinks() {
-    document.getElementById('maindishes').innerHTML = '';
-    document.getElementById('drinks').innerHTML = '';
-    document.getElementById('desserts').innerHTML = '';
-    renderDrinks();
+  document.getElementById("maindishes").innerHTML = "";
+  document.getElementById("drinks").innerHTML = "";
+  document.getElementById("desserts").innerHTML = "";
+  renderDrinks();
 }
 
 function onlyDesserts() {
-    document.getElementById('maindishes').innerHTML = '';
-    document.getElementById('drinks').innerHTML = '';
-    document.getElementById('desserts').innerHTML = '';
-    renderDesserts();
+  document.getElementById("maindishes").innerHTML = "";
+  document.getElementById("drinks").innerHTML = "";
+  document.getElementById("desserts").innerHTML = "";
+  renderDesserts();
 }
 
 function renderCart() {
-    let cartRef = document.getElementById('cart');
-    cartRef.innerHTML = '';
-    for (let i = 0; i < cart.length; i++) {
-        cartRef.innerHTML += getCartItem(i);
-    }
+  let cartRef = document.getElementById("cart");
+  cartRef.innerHTML = "";
+  for (let i = 0; i < cart.length; i++) {
+    cartRef.innerHTML += getCartItem(i);
+  }
 }
 
 function addToCart(i, startKey) {
-    let item = menu[startKey][i];
-    cart.push({
-        name: item.name,
-        price: item.price
-    });
-    renderCart();
+  let item = menu[startKey][i];
+  cart.push({
+    name: item.name,
+    price: item.price,
+  });
+  renderCart();
 }
 
 function removeFromCart(i) {
-    cart.splice(i, 1);
-    renderCart();
+  cart.splice(i, 1);
+  renderCart();
 }
--- scrpts/template.js
+++ scrpts/template.js
@@ -1,84 +1,96 @@
 function getMainDishes(i) {
-    return `
-        <button onclick="addToCart(${i}, 'mainDishes')" class="dish">
-            <div class="dish_header">
-                <h3>${menu.mainDishes[i].name}
-                </h3>
-                <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#FF8001">
-                <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>
-                </svg>
+return `
+<button onclick="addToCart(${i}, 'mainDishes')" class="dish">
+    <div class="dish_header">
+        <h3>${menu.mainDishes[i].name}
+        </h3>
+        <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#FF8001">
+            <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+        </svg>
 
-            </div><br>
-            <p>${menu.mainDishes[i].description}</p><br>
-            <div class="price">
-                <br><h3>€${menu.mainDishes[i].price.toFixed(2)}</h3>
-            </div>
-        </button>
-    `;
+    </div><br>
+    <p>${menu.mainDishes[i].description}</p><br>
+    <div class="price">
+        <br>
+        <h3>€${menu.mainDishes[i].price.toFixed(2)}</h3>
+    </div>
+</button>
+`;
 }
 
 function getDrinks(i) {
-    return `
-        <button onclick="addToCart(${i}, 'drinks')" class="dish">
-            <div class="dish_header">
-                <h3>${menu.drinks[i].name}
-                </h3>
-                <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#FF8001">
-                <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>
-                </svg>
+return `
+<button onclick="addToCart(${i}, 'drinks')" class="dish">
+    <div class="dish_header">
+        <h3>${menu.drinks[i].name}
+        </h3>
+        <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#FF8001">
+            <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+        </svg>
 
-            </div><br>
-            <p>${menu.drinks[i].description}</p><br>
-            <div class="price">
-                <br><h3>€${menu.drinks[i].price.toFixed(2)}</h3>
-            </div>
-        </button>
-    `;
+    </div><br>
+    <p>${menu.drinks[i].description}</p><br>
+    <div class="price">
+        <br>
+        <h3>€${menu.drinks[i].price.toFixed(2)}</h3>
+    </div>
+</button>
+`;
 }
 
 function getDesserts(i) {
-    return `
-        <button onclick="addToCart(${i}, 'desserts')" class="dish">
-            <div class="dish_header">
-                <h3>${menu.desserts[i].name}
-                </h3>
-                <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#FF8001">
-                <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>
-                </svg>
+return `
+<button onclick="addToCart(${i}, 'desserts')" class="dish">
+    <div class="dish_header">
+        <h3>${menu.desserts[i].name}
+        </h3>
+        <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#FF8001">
+            <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+        </svg>
 
-            </div><br>
-                <p>${menu.desserts[i].description}</p><br>
-            <div class="price">
-            <br><h3>€${menu.desserts[i].price.toFixed(2)}</h3>
-            </div>
-        </button>
-    `;
+    </div><br>
+    <p>${menu.desserts[i].description}</p><br>
+    <div class="price">
+        <br>
+        <h3>€${menu.desserts[i].price.toFixed(2)}</h3>
+    </div>
+</button>
+`;
 }
 
 function getCartItem(i) {
-    return `
-        <article class="cart_item">
-            <section class="item_details">
-            <p>${cart[i].name}</p>
-            <p>€${cart[i].price.toFixed(2)}</p>
-            
-            
-            </section>
-            <section class="item_details">
-            <div class="portions_control">
+return `
+<article class="cart_item">
+    <section class="item_details">
+        <p>${cart[i].name}</p>
+        <p>€${cart[i].price.toFixed(2)}</p>
+
+
+    </section>
+    <section class="item_details">
+        <div class="portions_control">
             <button>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF8001"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                    fill="#FF8001">
+                    <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+                </svg>
             </button>
-                <input type="number" id="portions" class="portions" value="1">
+            <input type="number" id="portions" class="portions" value="1">
             <button>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF8001"><path d="M200-440v-80h560v80H200Z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                    fill="#FF8001">
+                    <path d="M200-440v-80h560v80H200Z" />
+                </svg>
             </button>
-            </div>
-            <button onclick="removeFromCart(${i})">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF8001"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-            </button>
-            </section>
-            
-        </article>
-    `;
+        </div>
+        <button onclick="removeFromCart(${i})">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF8001">
+                <path
+                    d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+            </svg>
+        </button>
+    </section>
+
+</article>
+`;
 }
