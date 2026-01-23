82b434cd97cdfb191fc8ae2568552da1836a87cf
Jens Baumeister <jens.baumeister@gmx.net>
2026-01-23 15:19:08 +0100

    cart items styled

--- scrpts/template.js
+++ scrpts/template.js
@@ -57,12 +57,15 @@ function getDesserts(i) {
 
 function getCartItem(i) {
     return `
-        <section class="cart_item">
-            <button onclick="removeFromCart(${i})">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF8001"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-            </button>
-            <h5>${cart[i].name}</p><p>€${cart[i].price.toFixed(2)}<br></h5>
+        <article class="cart_item">
+            <section class="item_details">
+            <p>${cart[i].name}</p>
+            <p>€${cart[i].price.toFixed(2)}</p>
             
+            
+            </section>
+            <section class="item_details">
+            <div class="portions_control">
             <button>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF8001"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
             </button>
@@ -70,7 +73,12 @@ function getCartItem(i) {
             <button>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF8001"><path d="M200-440v-80h560v80H200Z"/></svg>
             </button>
+            </div>
+            <button onclick="removeFromCart(${i})">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FF8001"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+            </button>
+            </section>
             
-        </section>
+        </article>
     `;
 }
