754334ded74f5c82dddeea7cd3021980319c5d43
Jens Baumeister <jens.baumeister@gmx.net>
2026-02-26 12:04:44 +0100

    price displays now , instead .

--- scripts/template.js
+++ scripts/template.js
@@ -14,7 +14,7 @@ function getMenuItemTemplate(item, category, index) {
     <p>${item.description}</p><br>
     <div class="price">
         <br>
-        <h3>€${item.price.toFixed(2)}</h3>
+        <h3>€${item.price.toFixed(2).replace('.', ',')}</h3>
     </div>
 </section>
 `;
@@ -25,7 +25,7 @@ function getCartItem(i) {
 <article class="cart_item">
     <section class="item_details">
         <p>${cart[i].name}</p>
-        <p>€${(cart[i].price * cart[i].quantity).toFixed(2)}</p>
+        <p>€${(cart[i].price * cart[i].quantity).toFixed(2).replace('.', ',')}</p>
 
 
     </section>
@@ -60,34 +60,15 @@ function getTotalPrice(total, deliveryCost) {
   return `
 <section class="price_details">
     <p>
-        Lieferservice: €${deliveryCost.toFixed(2)}
+        Lieferservice: €${deliveryCost.toFixed(2).replace('.', ',')}
     </p>
-    <h4>Gesamt: €${(total + deliveryCost).toFixed(2)}</h4>
+    <h4>Gesamt: €${(total + deliveryCost).toFixed(2).replace('.', ',')}</h4>
 </section>
 <section class="orders_control">
 <button onclick="checkout()" class="checkout_button" aria-label="checkout">
     <p>Zur Kasse</p>
 </button>
 <button onclick="emptyCart()" class="emptycart_button" aria-label="empty cart">
-    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fffefe"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-</button>
-
-`;
-}
-
-function getMobileTotalPrice(total, deliveryCost) {
-  return `
-<section>
-    <p>
-        Lieferservice: €${deliveryCost.toFixed(2)}
-    </p>
-    <h4>Gesamt: €${(total + deliveryCost).toFixed(2)}</h4>
-</section>
-<section class="orders_control">
-<button onclick="checkout()" class="checkout_button">
-    <p>Zur Kasse</p>
-</button>
-<button onclick="emptyCart()" class="emptycart_button">
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fffefe"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
 </button>
 
